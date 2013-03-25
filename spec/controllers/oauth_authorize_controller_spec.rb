58ec6c9ba7120b4b35af8b8640c40287dc436209
Khurram Zaman <khurram.zaman@gmail.com>
2013-03-25 20:36:03 +0500

    Specs for client credentials grant added.

--- spec/controllers/oauth_authorize_controller_spec.rb
+++ spec/controllers/oauth_authorize_controller_spec.rb
@@ -4,7 +4,7 @@ describe OauthAuthorizeController do
   let(:user) { create(:user) }
   let(:oauth2_client) { create(:oauth2_client) }
 
-  describe 'auth ok' do
+  describe 'password grant auth ok' do
     it "returns a valid token with valid username and password" do
       client = OAuth2::Client.new(oauth2_client.client_id, oauth2_client.client_secret) do |b|
         b.request :url_encoded
@@ -15,13 +15,34 @@ describe OauthAuthorizeController do
     end
   end
 
-  describe 'auth nok' do
+  describe 'password grant auth nok' do
     it "return error with invalid username and password" do
       client = OAuth2::Client.new(oauth2_client.client_id, oauth2_client.client_secret) do |b|
         b.request :url_encoded
         b.adapter :rack, Rails.application
       end
       lambda {client.password.get_token(user.email, "123")}.should raise_error(OAuth2::Error)
+    end
+  end
+
+  describe 'client_credentials grant auth ok' do
+    it "returns a valid token with valid client credentials" do
+      client = OAuth2::Client.new(oauth2_client.client_id, oauth2_client.client_secret) do |b|
+        b.request :url_encoded
+        b.adapter :rack, Rails.application
+      end
+      token = client.client_credentials.get_token
+      token.should_not be_expired
+    end
+  end
+
+  describe 'client_credentials grant auth nok' do
+    it "return error with invalid client credentials" do
+      client = OAuth2::Client.new(oauth2_client.client_id, "123") do |b|
+        b.request :url_encoded
+        b.adapter :rack, Rails.application
+      end
+      lambda { client.client_credentials.get_token }.should raise_error(OAuth2::Error)
     end
   end
 end
--- spec/factories/oauth2_clients.rb
+++ spec/factories/oauth2_clients.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :oauth2_client, :class => Songkick::OAuth2::Model::Client do
     name { Faker::Lorem.characters(8) }
     redirect_uri { Faker::Internet.url }
+    owner
   end
 end
